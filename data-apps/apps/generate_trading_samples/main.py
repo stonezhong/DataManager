60fc3d9917bb8ff0e5984be9419347ecdc86cb42
Stone Zhong <stonezhong@hotmail.com>
2020-10-28 20:11:46 -0700

    adding loader field to dataset instance

--- data-apps/apps/generate_trading_samples/main.py
+++ data-apps/apps/generate_trading_samples/main.py
@@ -35,7 +35,7 @@ def register_dataset_instance(input_args, market, df):
     data_time = datetime.strptime(dt, "%Y-%m-%d")
     dcc.create_dataset_instance(
         'tradings', '1.0', 1,
-        f"/{dt}", [{
+        f"/{dt}_{market}", [{
             'type': 'parquet',
             'location': f'hdfs:///data/tradings/{dt}/{market}.parquet'
         }],
@@ -58,28 +58,55 @@ def main(spark, input_args):
     print(f"dt = {dt}")
 
     app_args = input_args['app_args']
-    market = app_args['market']
+    market = app_args.get('market')
 
-    random.seed()
+    if market:
+        random.seed()
+        Trade = Row("market", "type", "symbol", "amount", "price", "commission")
+        trades = []
+        for i in range(0, 1000):
+            type = ["BUY", "SELL"][random.randint(0, 1)]
+            symbol = list(STOCK_LIST.keys())[random.randint(0, len(STOCK_LIST)-1)]
+            amount = random.randint(1, 500)
+            rate = 1 + (random.random() - 0.5)/5.0
+            price = round(STOCK_LIST[symbol]*rate, 2)
+            commission = round(amount / 100.0 + 9.0, 2)
 
-    Trade = Row("market", "type", "symbol", "amount", "price", "commission")
+            trade = Trade(market, type, symbol, amount, price, commission)
+            trades.append(trade)
 
-    trades = []
-    for i in range(0, 1000):
-        type = ["BUY", "SELL"][random.randint(0, 1)]
-        symbol = list(STOCK_LIST.keys())[random.randint(0, len(STOCK_LIST)-1)]
-        amount = random.randint(1, 500)
-        rate = 1 + (random.random() - 0.5)/5.0
-        price = round(STOCK_LIST[symbol]*rate, 2)
-        commission = round(amount / 100.0 + 9.0, 2)
+        df = spark.createDataFrame(trades)
+        df.write.mode("overwrite").parquet(f"/data/tradings/{dt}/{market}.parquet")
 
-        trade = Trade(market, type, symbol, amount, price, commission)
-        trades.append(trade)
+        print(f"Writing to /data/tradings/{dt}/{market}.parquet")
 
-    df = spark.createDataFrame(trades)
-    df.write.mode("overwrite").parquet(f"/data/tradings/{dt}/{market}.parquet")
+        register_dataset_instance(input_args, market, df)
+    else:
+        # register the view after all the market are uploaded
+        dc_config = input_args['dc_config']
+        pipeline_group_context = input_args['pipeline_group_context']
+        dt = pipeline_group_context['dt']
 
-    print(f"Writing to /data/tradings/{dt}/{market}.parquet")
+        dcc = DataCatalogClient(
+            url_base = dc_config['url_base'],
+            auth = (dc_config['username'], dc_config['password'])
+        )
 
-    register_dataset_instance(input_args, market, df)
+
+        data_time = datetime.strptime(dt, "%Y-%m-%d")
+        dcc.create_dataset_instance(
+            'tradings', '1.0', 1,
+            f"/{dt}", [],
+            data_time,
+            loader = json.dumps({
+                "name": "union",
+                "args": {
+                    "dsi_paths": [
+                        "tradings:1.0:1:{dt}_NASDAQ",
+                        "tradings:1.0:1:{dt}_NYSE",
+                    ]
+                }
+            })
+        )
+
     print("Done")
