98026d91df3e244a6902be837dfeef9a7eed71cc
Stone Zhong <stonezhong@hotmail.com>
2020-11-08 06:04:08 -0800

    redirect home page to datasets UI

--- server/DataCatalog/urls.py
+++ server/DataCatalog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.urls import path
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/explorer/datasets')),  # hack, avoid extra redirect
     path('admin/', admin.site.urls),
     url(r'^api/', include('main.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
