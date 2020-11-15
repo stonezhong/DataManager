fcfd208100aacbc7c89932a208ededb64118f931
Stone Zhong <stonezhong@hotmail.com>
2020-11-15 15:26:52 -0800

    refresh table after modification

--- server/explorer/client/pages/datasets/main.jsx
+++ server/explorer/client/pages/datasets/main.jsx
@@ -18,97 +18,53 @@ class DatasetsPage extends React.Component {
         showExpired: false
     };
 
-    // onSave = (mode, dataset) => {
-    //     if (mode == "new") {
-    //         // TODO: shuold not trust client side time
-    //         const now = dt_2_utc_string(new Date());
-    //         const to_post = {
-    //             name            : dataset.name,
-    //             major_version   : dataset.major_version,
-    //             minor_version   : parseInt(dataset.minor_version),
-    //             description     : dataset.description,
-    //             team            : dataset.team,
-    //             publish_time    : now,
-    //         }
+    onSave = (mode, dataset) => {
+        if (mode == "new") {
+            // TODO: shuold not trust client side time
+            const now = dt_2_utc_string(new Date());
+            const to_post = {
+                name            : dataset.name,
+                major_version   : dataset.major_version,
+                minor_version   : parseInt(dataset.minor_version),
+                description     : dataset.description,
+                team            : dataset.team,
+                publish_time    : now,
+            }
 
-    //         fetch('/api/Datasets/', {
-    //             method: 'post',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'X-CSRFToken': get_csrf_token(),
-    //             },
-    //             body: JSON.stringify(to_post)
-    //         })
-    //             .then((res) => res.json())
-    //             .catch(() => {
-    //                 this.theTopMessageRef.current.show("danger", "Unable to save!");
-    //             })
-    //             .then(
-    //                 (result) => {
-    //                     this.load_datasets();
-    //                 }
-    //             )
-    //     } else if (mode == 'edit') {
-    //         // You can only change description and team
-    //         const to_patch = {
-    //             description     : dataset.description,
-    //             team            : dataset.team,
-    //             expiration_time : (dataset.expiration_time==='')?null:dataset.expiration_time
-    //         }
-    //         fetch(`/api/Datasets/${dataset.id}/`, {
-    //             method: 'patch',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'X-CSRFToken': get_csrf_token(),
-    //             },
-    //             body: JSON.stringify(to_patch)
-    //         })
-    //             .then((res) => res.json())
-    //             .catch(() => {
-    //                 this.theTopMessageRef.current.show("danger", "Unable to save!");
-    //             })
-    //             .then(
-    //                 (result) => {
-    //                     this.load_datasets();
-    //                 }
-    //             )
-    //     }
-    // };
+            return fetch('/api/Datasets/', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': get_csrf_token(),
+                },
+                body: JSON.stringify(to_post)
+            })
+                .then((res) => res.json())
+                .catch(() => {
+                    this.theTopMessageRef.current.show("danger", "Unable to save!");
+                })
+        } else if (mode == 'edit') {
+            // You can only change description and team
+            const to_patch = {
+                description     : dataset.description,
+                team            : dataset.team,
+                expiration_time : (dataset.expiration_time==='')?null:dataset.expiration_time
+            }
+            return fetch(`/api/Datasets/${dataset.id}/`, {
+                method: 'patch',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': get_csrf_token(),
+                },
+                body: JSON.stringify(to_patch)
+            })
+                .then((res) => res.json())
+                .catch(() => {
+                    this.theTopMessageRef.current.show("danger", "Unable to save!");
+                })
+        }
+    };
 
-    // load_datasets = () => {
-    //     const buildArgs = {
-    //         path: "/api/Datasets/",
-    //         queryParams: {
-    //             ordering: '-publish_time',
-    //         }
-    //     };
-    //     if (!this.state.showExpired) {
-    //         buildArgs.queryParams.expiration_time__isnull="True"
-    //     }
-    //     const url = buildUrl('', buildArgs);
-
-    //     fetch(url)
-    //         .then(res => res.json())
-    //         .then(
-    //             (result) => {
-    //                 this.setState({datasets: result})
-    //             }
-    //         )
-    //         .catch(() => {
-    //             this.theTopMessageRef.current.show("danger", "Unable to list datasets!");
-    //         })
-    // }
-
-
-    componentDidMount() {
-        // this.load_datasets();
-    }
-
-    // onShowExpiredChange = (showExpired) => {
-    //     this.setState({showExpired: showExpired}, () => {
-    //         this.load_datasets();
-    //     });
-    // };
 
     get_page = (offset, limit, filter={}) => {
         const buildArgs = {
