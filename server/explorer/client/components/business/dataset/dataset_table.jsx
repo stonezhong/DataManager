bc93d639a6b73f24794433e91563544a25f07750
Stone Zhong <stonezhong@hotmail.com>
2020-11-15 14:46:27 -0800

    add paging

--- server/explorer/client/components/business/dataset/dataset_table.jsx
+++ server/explorer/client/components/business/dataset/dataset_table.jsx
@@ -43,10 +43,58 @@ export class DatasetTable extends React.Component {
     theDatasetEditorRef = React.createRef();
     theSchemaViewerRef  = React.createRef();
 
+    get_schema = dataset => {
+        if (!dataset.schema) {
+            return null;
+        }
+        const schema_str = dataset.schema.trim();
+        if (!schema_str) {
+            return null;
+        }
+        return JSON.parse(schema_str);
+    };
+
+    render_schema = dataset => {
+        return (
+            this.get_schema(dataset) &&
+            <Button
+                variant="secondary"
+                size="sm"
+                onClick={
+                    event => {
+                        this.theSchemaViewerRef.current.openDialog(
+                            this.get_schema(dataset)
+                        )
+                    }
+                }
+            >
+                <Icon.Info />
+            </Button>
+        );
+    };
+
+    render_tools = dataset => {
+        return (
+            <Button
+                variant="secondary"
+                size="sm"
+                onClick={
+                    event => {
+                        this.theDatasetEditorRef.current.openDialog(
+                            this.props.allowEdit?"edit":"view", dataset
+                        )
+                    }
+                }
+            >
+                { this.props.allowEdit?<Icon.Pencil />:<Icon.Info />}
+            </Button>
+        );
+    };
+
     columns = {
-        tools:          {display: ""},
+        tools:          {display: "", render_data: this.render_tools},
         name:           {display: "Name"},
-        schema:         {display: "Schema", render_data: row => null},
+        schema:         {display: "Schema", render_data: this.render_schema},
         author:         {display: "Author"},
         team:           {display: "Team"},
         published:      {display: "Published"},
