568f4c656d62706ca77c5473163ca55a141f5889
Stone Zhong <stonezhong@hotmail.com>
2020-11-22 21:36:20 -0800

    improve scheduler ui

--- server/explorer/client/pages/schedulers/main.jsx
+++ server/explorer/client/pages/schedulers/main.jsx
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container'
 import {TimerTable} from '/components/business/timer/main.jsx'
 
 import $ from 'jquery'
+const buildUrl = require('build-url');
 
 import {get_csrf_token, get_current_user} from '/common_lib'
 
@@ -30,20 +31,14 @@ class SchedulersPage extends React.Component {
                 end_at          : timer.end_at,
             }
 
-            fetch('/api/Timers/', {
+            return fetch('/api/Timers/', {
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
                     'X-CSRFToken': get_csrf_token(),
                 },
                 body: JSON.stringify(to_post)
-            })
-                .then((res) => res.json())
-                .then(
-                    (result) => {
-                        this.load_timers();
-                    }
-                )
+            }).then((res) => res.json())
         } else if (mode === "edit") {
             const to_patch = {
                 description     : timer.description,
@@ -58,37 +53,30 @@ class SchedulersPage extends React.Component {
                 category        : timer.category,
             }
 
-            fetch(`/api/Timers/${timer.id}/`, {
+            return fetch(`/api/Timers/${timer.id}/`, {
                 method: 'patch',
                 headers: {
                     'Content-Type': 'application/json',
                     'X-CSRFToken': get_csrf_token(),
                 },
                 body: JSON.stringify(to_patch)
-            })
-                .then((res) => res.json())
-                .then(
-                    (result) => {
-                        this.load_timers();
-                    }
-                )
+            }).then((res) => res.json())
         }
     };
 
-    load_timers() {
-        // we only show pipeline related timers
-        fetch("/api/Timers/?topic=pipeline")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                this.setState({timers: result.results})
+    get_page = (offset, limit, filter={}) => {
+        const buildArgs = {
+            path: "/api/Timers/",
+            queryParams: {
+                offset: offset,
+                limit : limit,
+                topic: 'pipeline',
             }
-        )
-    }
+        };
+        const url = buildUrl('', buildArgs);
+        return fetch(url).then(res => res.json());
 
-    componentDidMount() {
-        this.load_timers();
-    }
+    };
 
     render() {
         return (
@@ -96,8 +84,10 @@ class SchedulersPage extends React.Component {
                 <TimerTable
                     allowEdit={!!this.props.current_user}
                     allowNew={!!this.props.current_user}
-                    timers={this.state.timers}
                     onSave={this.onSave}
+                    get_page={this.get_page}
+                    page_size={15}
+                    size="sm"
                 />
             </Container>
         )
