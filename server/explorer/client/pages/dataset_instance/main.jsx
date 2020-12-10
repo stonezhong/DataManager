57ddddd432021d4073b466d5497ecc9599e40b6a
Stone Zhong <stonezhong@hotmail.com>
2020-12-10 08:08:03 -0800

    improve SQL-step UI

--- server/explorer/client/pages/dataset_instance/main.jsx
+++ server/explorer/client/pages/dataset_instance/main.jsx
@@ -9,19 +9,45 @@ import $ from 'jquery'
 
 import {get_current_user, get_app_context} from '/common_lib'
 import {DatasetInstanceView} from '/components/business/dataset/dataset_instance_revision.jsx'
+import {DatasetLink} from '/components/business/dataset/utils.jsx'
 
+import "./page.scss"
+
+/*********************************************************************************
+ * Purpose: Show a single asset revision
+ *
+ * Props
+ *     dataset              : A dataset object
+ *     dataset_instance     : A dataset instance object
+ *     execute_sql_app_id   : application_id for Execute SQL statement
+ *
+ */
 class DatasetInstancePage extends React.Component {
     render() {
         return (
             <Container fluid>
                 <Row>
-                    <Col>
+                    <Col xs={6}>
                         <h1>
                             Asset
                         </h1>
+                        <table className="asset-general-info-table">
+                            <tbody>
+                                <tr>
+                                    <td>Dataset</td>
+                                    <td><DatasetLink ds={this.props.dataset} /></td>
+                                </tr>
+                                <tr>
+                                    <td>Full Path</td>
+                                    <td>
+                                        {this.props.dsi_path}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </Col>
                 </Row>
-                <Row>
+                <Row className="mt-2">
                     <Col>
                     {
                         this.props.dsi_list.map(dsi => <div id={`revision-${dsi.revision}`}  className="mb-4">
