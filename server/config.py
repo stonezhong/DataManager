5e91235a50f17efa758aaf0fa1db29ab86d31db0
Stone Zhong <stonezhong@hotmail.com>
2020-12-21 03:49:48 -0800

    allow unit test in dev machine

--- server/config.py
+++ server/config.py
@@ -4,9 +4,17 @@ import os
 import json
 
 APP_NAME = "dm"
-ENV_HOME = os.environ['ENV_HOME']
+ENV_HOME = os.environ.get('ENV_HOME')
 
 def get_mordor_config(name):
+    if os.environ.get('DM_DJANGO_TEST') == '1':
+        config_filename = os.path.join(
+            os.path.expanduser("~/.dmtest/server/configs"),
+            name
+        )
+        with open(config_filename, "rt") as f:
+            return f.read()
+
     with open(os.path.join(ENV_HOME, "configs", APP_NAME, name), "rt") as f:
         return f.read()
 
@@ -19,4 +27,6 @@ def get_mordor_config_json_template(name, context={}):
     return json.loads(rendered_content)
 
 def get_log_path():
+    if os.environ.get('DM_DJANGO_TEST') == '1':
+        return os.path.expanduser("~/.dmtest/server/logs")
     return os.path.join(ENV_HOME, "logs", APP_NAME)
