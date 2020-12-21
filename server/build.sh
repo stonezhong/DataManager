5218f9b1b760361f1425c034cbe45fb63d7e4d4c
Stone Zhong <stonezhong@hotmail.com>
2020-12-21 00:59:28 -0800

    fix typo in build script

--- server/build.sh
+++ server/build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-source ~/.dmbuild/server/settings
+. ~/.dmbuild/server/settings
 
-DM_STAGE=beta
+echo "*********************************"
+echo "*                               *"
+echo "* build for stage: ${DM_STAGE}         *"
+echo "*                               *"
+echo "*********************************"
 
 pysassc explorer/templates/css/main.scss                explorer/static/css/main.css
 npm run build-prod
 # npm run build-dev
 
-mordor -a stage -p dm --stage $DM_STAGE --update-venv F
-
+mordor -a stage -p dm --stage ${DM_STAGE} --update-venv F
